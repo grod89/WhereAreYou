9d535f8c00572ee1cb39d02a5a7e50abef6396e8
Gerrid Rose <grod_89@hotmail.com>
2014-04-12 19:59:31 -0500

    Added GET ability so that when you type in a phone number it will get that location of the device from the server.  Right now phones and devices that do not return a valid phone number result in a default value of 55555. Also, only when the device gets a new marker to place will it switch to that location with the camera and then the camera can be moved back.

--- src/com/android/whereareyou/MapActivity.java
+++ src/com/android/whereareyou/MapActivity.java
@@ -2,6 +2,7 @@ package com.android.whereareyou;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
@@ -21,12 +22,15 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -46,7 +50,9 @@ import android.os.Handler;
 import android.os.SystemClock;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
@@ -58,29 +64,49 @@ public class MapActivity extends Activity {
  private Location userLocation;
  
  private LatLng userLocationLatLng = null;
+ private LatLng phoneLatLng = null;
+ 
+ String recipientPhoneNumber = null;
+ String userPhoneNumber = null;
  
  boolean selfMarkerCreated = false;
+ boolean otherMarkerCreated = false;
  Marker userMarkerLocation = null;
+ 
+ Marker otherMarkerLocation = null;
  
  LocationManager myLocationManager;
  String PROVIDER = LocationManager.GPS_PROVIDER;
   
  @Override
  protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_map);
-  getActionBar().setDisplayHomeAsUpEnabled(true);
-   
-  myLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-   
-  //get last known location, if available
-  Location lastKnownLoc = myLocationManager.getLastKnownLocation(PROVIDER);
-  userLocation = lastKnownLoc;
-  
-  // create the LatLng object needed for gmap
-  userLocationLatLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
-  
-  showMyLocation(lastKnownLoc);
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.activity_map);
+	  getActionBar().setDisplayHomeAsUpEnabled(true);
+	  
+		// Get the message from the intent
+		Intent intent = getIntent();
+		recipientPhoneNumber = intent.getStringExtra(HomeActivity.EDIT_TEXT1);
+		
+		
+		TelephonyManager tMgr =(TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+	    userPhoneNumber = tMgr.getLine1Number();
+	    System.out.println("Returned phone number = " + userPhoneNumber);
+	    if(userPhoneNumber == null) {
+	    	userPhoneNumber = "55555";
+	    	System.out.println("Null phone number returned, setting to default value of 55555");
+	    }
+		
+	  myLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	   
+	  //get last known location, if available
+	  Location lastKnownLoc = myLocationManager.getLastKnownLocation(PROVIDER);
+	  userLocation = lastKnownLoc;
+	  
+	  // create the LatLng object needed for gmap
+	  userLocationLatLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
+	  
+	  showMyLocation(lastKnownLoc);
  }
   
  @Override
@@ -112,13 +138,31 @@ public class MapActivity extends Activity {
 	  LatLng markerLoc = userLocationLatLng;
 	  DoPOST mDoPOST = new DoPOST(MapActivity.this);
 	  mDoPOST.execute("");
+	  DoGET mDoGET = new DoGET(MapActivity.this);
+	  mDoGET.execute("");
+	  
 	  if (selfMarkerCreated) {
 		  animateMarker(userMarkerLocation, markerLoc, false);
 	  } else {
+		  System.out.println("hello");
 		  userMarkerLocation = gMap.addMarker(new MarkerOptions().position(markerLoc).title("User Location!"));
-		  gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLoc, 15));
 		  selfMarkerCreated = true;
+		  gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocationLatLng, 15));
 	  }
+	  
+	  // see if user entered a phone number recipient to watch, if so then create and move to that marker
+	  if (recipientPhoneNumber != null && !recipientPhoneNumber.isEmpty() && phoneLatLng != null) {
+		  if (otherMarkerCreated){
+			  animateMarker(otherMarkerLocation, phoneLatLng, false);
+		  } else {
+			  System.out.println("hello2");
+			  System.out.println(recipientPhoneNumber);
+			  otherMarkerLocation = gMap.addMarker(new MarkerOptions().position(phoneLatLng).title("Other user's location!"));
+			  gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(phoneLatLng, 15));
+			  otherMarkerCreated = true;
+		  }
+	  }
+	  
   } 
  }
  
@@ -157,6 +201,143 @@ public class MapActivity extends Activity {
 	 
  }
  
+ public class DoGET extends AsyncTask<String, Void, Boolean> {
+
+	 Context mContext = null;
+	 
+	 //Result data
+	 double latitudeOfOther;
+	 double longitudeOfOther;
+	 
+	 Exception exception = null;
+	 
+	 String phoneNumOfOther, latitude, longitude = null;
+	 
+	 InputStream is = null;
+	 
+	 String url = "http://192.168.1.76/whereareyouserver/httpget.php";
+	 
+	 JSONObject jObj = null;
+	 String json="";
+	 
+    JSONArray numLocArray = null;
+	 
+	// JSON Node names
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_NUMLOC = "numLoc";
+	 
+	 DoGET(Context context) {
+		 mContext = context;
+	 }
+	 
+	@Override
+	protected Boolean doInBackground(String... arg0) {
+		
+		// Defined URL  where to send data
+		HttpClient httpclient = new DefaultHttpClient();
+		
+		phoneNumOfOther = getPhoneNumber();
+		
+		try {
+			
+			//Add data
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+			nameValuePairs.add(new BasicNameValuePair("serial", getPhoneNumber()));
+			String paramString = URLEncodedUtils.format(nameValuePairs, "utf-8");
+			url += "?" + paramString;
+			HttpGet httpget = new HttpGet(url);
+			
+			HttpResponse httpresponse = httpclient.execute(httpget);
+			HttpEntity httpentity = httpresponse.getEntity();
+			is = httpentity.getContent();
+			System.out.println("Sending out GET!");
+			System.out.println("Using serial = " + phoneNumOfOther);
+			
+		} catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+ 
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+        
+        try {
+        	System.out.println(jObj.toString());
+        	
+			// Checking for SUCCESS TAG
+			int success = jObj.getInt(TAG_SUCCESS);
+			 
+			if (success == 1) {
+				numLocArray = jObj.getJSONArray(TAG_NUMLOC);
+			 
+			    // looping through All Products
+				JSONObject jObj = numLocArray.getJSONObject(0);
+	        	System.out.println("GET numLoc json object = " + jObj.toString());
+				setLatitudeOfOther(jObj.getDouble("latitude"));
+				setLongitudeOfOther(jObj.getDouble("longitude"));
+			} else {
+				System.out.println("no numLoc found in received JSON object from server");
+			}
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        LatLng otherLatLng = new LatLng(getLatitudeOfOther(), getLongitudeOfOther());
+        
+        phoneLatLng = otherLatLng;
+
+		return true;
+	}
+	
+	protected void onGetExecute(Boolean valid) {
+		//Update the UI here
+	}
+	
+	public String getPhoneNumber() {
+		return recipientPhoneNumber;
+	}
+	
+	public double getLatitudeOfOther() {
+		return latitudeOfOther;
+	}
+	
+	public double getLongitudeOfOther() {
+		return longitudeOfOther;
+	}
+	
+	public void setLatitudeOfOther(double lat) {
+		latitudeOfOther = lat;
+	}
+	
+	public void setLongitudeOfOther(double lon) {
+		longitudeOfOther = lon;
+	}
+	 
+ }
+ 
  public class DoPOST extends AsyncTask<String, Void, Boolean> {
 
 	 Context mContext = null;
@@ -168,7 +349,7 @@ public class MapActivity extends Activity {
 	 
 	 Exception exception = null;
 	 
-	 String serial, latitude, longitude = null;
+	 String userPhoneNumberPost, latitude, longitude = null;
 	 
 	 DoPOST(Context context) {
 		 mContext = context;
@@ -181,17 +362,13 @@ public class MapActivity extends Activity {
 		HttpClient httpclient = new DefaultHttpClient();
 		HttpPost httppost = new HttpPost("http://192.168.1.76/whereareyouserver/httppost.php");
 		
-		serial = getSerial();
-		// FIX-ME temporary hack for serial variable
-		serial = "33333";
-		
 		latitude = String.valueOf(getLat());
 		longitude = String.valueOf(getLon());
 		try {
 			
 			//Add data
 			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
-			nameValuePairs.add(new BasicNameValuePair("serial", getSerial()));
+			nameValuePairs.add(new BasicNameValuePair("serial", getPhoneNumber()));
 			nameValuePairs.add(new BasicNameValuePair("latitude", latitude));
 			nameValuePairs.add(new BasicNameValuePair("longitude", longitude));
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
@@ -201,6 +378,8 @@ public class MapActivity extends Activity {
 			
 			//Execute HTTP Post Request
 			HttpResponse response = httpclient.execute(httppost);
+			
+			System.out.println(response.toString());
 			
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
@@ -216,8 +395,9 @@ public class MapActivity extends Activity {
 		//Update the UI here
 	}
 	
-	public String getSerial() {
-		return serial;
+	public String getPhoneNumber() {
+		userPhoneNumberPost = userPhoneNumber;
+		return userPhoneNumberPost;
 	}
 	
 	public double getLat() {
